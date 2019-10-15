63963b52f0de963e904836ab0fd47edb554410ef
Shailendra9k <learning4succedding@gmail.com>
2019-10-16 02:09:36 +0300

    project initiated

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState }  from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+
+function App() {
+  const [todo, setTodo] = useState({desc: '', date: ''});
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, todo]);
+  }
+
+  const inputChanged = (event) => {
+    setTodo({...todo, [event.target.name]: event.target.value});
+  } 
+
+  return (
+    <div className="App">
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            My Todolist
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      
+        <TextField style={{marginRight:10}} type='date' name='date' value={todo.date} onChange={inputChanged} />
+        <TextField type="text" name="desc" value={todo.desc} onChange={inputChanged}/>
+        <Button type="submit"  variant="contained" color="primary" size="medium" onClick={addTodo}>Add<Button/>
+        <table><tbody>
+          {
+            todos.map((todo, index) => 
+              <tr key={index}>
+                <td>{todo.date}</td>
+                <td>{todo.desc}</td>
+              </tr>
+            }
+        </tbody></table>
+    </div>
+  );
+}
+
+export default App;
