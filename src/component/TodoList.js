a6b88b68c55cba1dfd26618c4efbe76f323b9586
Shailendra Kushwaha <learning4succedding@gmail.com>
2019-10-16 04:11:57 +0300

    exercise-7 modified

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,33 +1,56 @@
 import React, { useState } from "react";
 
 function TodoList() {
-  const [desc, setDesc] = useState("");
+  const [desc, setDesc] = useState({
+    description: "",
+    date: ""
+  });
   const [todos, setTodos] = useState([]);
 
   const inputChanged = event => {
-    setDesc(event.target.value);
+    setDesc({ ...desc, [event.target.name]: event.target.value });
   };
 
   const addTodo = event => {
     event.preventDefault();
     setTodos([...todos, desc]);
+    setDesc([""]);
   };
 
-  const todosList = todos.map((todo, id) => (
-    <tr key={id}>
-      <td>{todo}</td>
+  const todosList = todos.map((todo, index) => (
+    <tr key={index}>
+      <td>{todo.date}</td>
+      <td>{todo.description}</td>
     </tr>
   ));
 
   return (
     <div>
       <form onSubmit={addTodo}>
-        <input type="text" onChange={inputChanged} value={desc} />
+        <label>Description:</label>
+        <input
+          type="text"
+          name="desc"
+          onChange={inputChanged}
+          value={desc.description}
+        />
+        <input
+          type="text"
+          name="date"
+          onChange={inputChanged}
+          value={desc.date}
+        />
         <input type="submit" value="Add" />
       </form>
 
       <table>
-        <tbody>{todosList}</tbody>
+        <tbody>
+          <tr>
+            <td>Date</td>
+            <td>Description</td>
+          </tr>
+          {todosList}
+        </tbody>
       </table>
     </div>
   );
